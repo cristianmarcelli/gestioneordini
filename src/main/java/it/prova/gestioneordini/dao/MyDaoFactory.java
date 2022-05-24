ec225b2736abe2ad4ebe4a2b67efec25c0a05ebb
Cristian <cristianma98@gmail.com>
2022-05-24 16:00:57 +0200

    aggiornata la classe MyDaoFactory con i relativi import dei dao

--- src/main/java/it/prova/gestioneordini/dao/MyDaoFactory.java
+++ src/main/java/it/prova/gestioneordini/dao/MyDaoFactory.java
@@ -1,30 +1,37 @@
 package it.prova.gestioneordini.dao;
 
+import it.prova.gestioneordini.dao.articolo.ArticoloDAO;
+import it.prova.gestioneordini.dao.articolo.ArticoloDAOImpl;
+import it.prova.gestioneordini.dao.categoria.CategoriaDAO;
+import it.prova.gestioneordini.dao.categoria.CategoriaDAOImpl;
+import it.prova.gestioneordini.dao.ordine.OrdineDAO;
+import it.prova.gestioneordini.dao.ordine.OrdineDAOImpl;
+
 public class MyDaoFactory {
 
-//	private static OrdineDAO ordineDaoInstance = null;
-//	private static ArticoloDAO articoloDaoInstance = null;
-//	private static CategoriaDAO categoriaDaoInstance = null;
-//
-//	public static OrdineDAO getOrdineDAOInstance() {
-//		if (ordineDaoInstance == null)
-//			ordineDaoInstance = new OrdineDAOImpl();
-//
-//		return ordineDaoInstance;
-//	}
-//
-//	public static ArticoloDAO getArticoloDAOInstance() {
-//		if (articoloDaoInstance == null)
-//			articoloDaoInstance = new ArticoloDAOImpl();
-//
-//		return articoloDaoInstance;
-//	}
-//
-//	public static CategoriaDAO getCategoriaDAOInstance() {
-//		if (categoriaDaoInstance == null)
-//			categoriaDaoInstance = new CategoriaDAOImpl();
-//
-//		return categoriaDaoInstance;
-//	}
+	private static OrdineDAO ordineDaoInstance = null;
+	private static ArticoloDAO articoloDaoInstance = null;
+	private static CategoriaDAO categoriaDaoInstance = null;
+
+	public static OrdineDAO getOrdineDAOInstance() {
+		if (ordineDaoInstance == null)
+			ordineDaoInstance = new OrdineDAOImpl();
+
+		return ordineDaoInstance;
+	}
+
+	public static ArticoloDAO getArticoloDAOInstance() {
+		if (articoloDaoInstance == null)
+			articoloDaoInstance = new ArticoloDAOImpl();
+
+		return articoloDaoInstance;
+	}
+
+	public static CategoriaDAO getCategoriaDAOInstance() {
+		if (categoriaDaoInstance == null)
+			categoriaDaoInstance = new CategoriaDAOImpl();
+
+		return categoriaDaoInstance;
+	}
 
 }
