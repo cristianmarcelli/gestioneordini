aa97d8b5ca30cbdae04b603a98ff530b9b856dd8
Cristian <cristianma98@gmail.com>
2022-05-24 15:03:26 +0200

    creato package it.prova.gestioneordini.dao

---
+++ src/main/java/it/prova/gestioneordini/dao/MyDaoFactory.java
@@ -0,0 +1,30 @@
+package it.prova.gestioneordini.dao;
+
+public class MyDaoFactory {
+
+//	private static OrdineDAO ordineDaoInstance = null;
+//	private static ArticoloDAO articoloDaoInstance = null;
+//	private static CategoriaDAO categoriaDaoInstance = null;
+//
+//	public static OrdineDAO getOrdineDAOInstance() {
+//		if (ordineDaoInstance == null)
+//			ordineDaoInstance = new OrdineDAOImpl();
+//
+//		return ordineDaoInstance;
+//	}
+//
+//	public static ArticoloDAO getArticoloDAOInstance() {
+//		if (articoloDaoInstance == null)
+//			articoloDaoInstance = new ArticoloDAOImpl();
+//
+//		return articoloDaoInstance;
+//	}
+//
+//	public static CategoriaDAO getCategoriaDAOInstance() {
+//		if (categoriaDaoInstance == null)
+//			categoriaDaoInstance = new CategoriaDAOImpl();
+//
+//		return categoriaDaoInstance;
+//	}
+
+}
